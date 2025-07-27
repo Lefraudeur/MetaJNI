fdbfaa426240fb1064b49d00514dbc9cd29170d5
Lefraudeur <91006387+Lefraudeur@users.noreply.github.com>
2025-07-27 21:26:18 +0200

    removed logging on release build

--- src/logger/logger.cpp
+++ src/logger/logger.cpp
@@ -1,26 +1,36 @@
 #include "logger.hpp"
 
-static std::ofstream logfile;
+#ifndef NDEBUG
+static std::ofstream logfile
+#endif
 
 bool logger::init()
 {
+#ifndef NDEBUG
 	logfile = std::ofstream("mujina_logs.txt");
 	if (!logfile)
 		return false;
+#endif
 	return true;
 }
 
 void logger::shutdown()
 {
+#ifndef NDEBUG
 	logfile = std::ofstream();
+#endif
 }
 
 void logger::log(std::string_view msg)
 {
+#ifndef NDEBUG
 	logfile << "info: " << msg << std::endl;
+#endif NDEBUG
 }
 
 void logger::error(std::string_view msg)
 {
+#ifndef NDEBUG
 	logfile << "error: " << msg << std::endl;
+#endif
 }
